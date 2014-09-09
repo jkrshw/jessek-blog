e85dee6b9d3fae19ca4313784833813504c2a99d
Jesse Kershaw <jesse.kershaw@gmail.com>
2014-09-09 23:23:41 +1200

    Use spaces instead of tabs before anyone notices!

--- deploy/simple-sync.py
+++ deploy/simple-sync.py
@@ -10,8 +10,8 @@ import os
 
 # read in the hash file. Keyed by hash
 def read_hash(filename):
-	with open(filename) as f:
-		return dict([line.split() for line in f])
+    with open(filename) as f:
+        return dict([line.split() for line in f])
 
 #########  main program ###########
 os.system("wget -q -O old_images.md5 http://jessek.co.nz/images.md5")
@@ -21,5 +21,5 @@ new_hashes = read_hash("output/images.md5")
 old_hashes = read_hash("old_images.md5")
 
 for key in new_hashes.viewkeys() & old_hashes.viewkeys():
-	print 'removing unchanged file %s' % new_hashes[key]
-	os.remove(new_hashes[key])
+    print 'removing unchanged file %s' % new_hashes[key]
+    os.remove(new_hashes[key])
